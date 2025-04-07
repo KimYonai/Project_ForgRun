3e30ccb3e90db8e69548feb3c6cb779992122ac6
YunHwan Kim <89112959+KimYonai@users.noreply.github.com>
2025-04-07 10:56:16 +0900

    [Create] 캐릭터 조작 상태 패턴 기본 틀 설계

--- Assets/Scripts/Character/Test/StateManager.cs
+++ Assets/Scripts/Character/Test/StateManager.cs
@@ -1,10 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public enum PlayerState { Idle, Jump, Slide, Die }      // 플레이어의 상태 열거형
 
 public class StateManager : MonoBehaviour
 {
-    private PlayerState state;
+    private PlayerState state;          // 플레이어의 상태 열거형을 받아오기 위한 변수
+    private Coroutine currentState;     // 현재 상태 코루틴 변수
 
     private void Awake()
     {
@@ -19,31 +21,6 @@ public class StateManager : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.W)) { ChangeState(PlayerState.Jump); }
         else if (Input.GetKeyDown(KeyCode.E)) { ChangeState(PlayerState.Slide); }
         else if (Input.GetKeyDown(KeyCode.R)) {  ChangeState(PlayerState.Die); }
-
-        UpdateState();
-    }
-
-    /// <summary>
-    /// 플레이어의 상태 업데이트용 함수
-    /// PlayerState에 따라 현재 플레이어의 행동 실행
-    /// </summary>
-    private void UpdateState()
-    {
-        switch (state)
-        {
-            case PlayerState.Idle:
-                Debug.Log("Idle State");
-                break;
-            case PlayerState.Jump:
-                Debug.Log("Jump State");
-                break;
-            case PlayerState.Slide:
-                Debug.Log("Slide State");
-                break;
-            case PlayerState.Die:
-                Debug.Log("Die State");
-                break;
-        }
     }
 
     /// <summary>
@@ -52,6 +29,52 @@ public class StateManager : MonoBehaviour
     /// <param name="newState"></param>
     private void ChangeState(PlayerState newState)
     {
+        if (currentState != null) { StopCoroutine(currentState); }
         state = newState;
+        currentState = StartCoroutine(state.ToString());
+    }
+
+    private IEnumerator Idle()
+    {
+        Debug.Log("<color=Green>Change State : Idle</color>");
+
+        while (true)
+        {
+            Debug.Log("Idle State");
+            yield return null;
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        Debug.Log("<color=Yellow>Change State : Jump</color>");
+
+        while (true)
+        {
+            Debug.Log("Jump State");
+            yield return null;
+        }
+    }
+
+    private IEnumerator Slide()
+    {
+        Debug.Log("<color=Orange>Change State : Slide</color>");
+
+        while (true)
+        {
+            Debug.Log("Slide State");
+            yield return null;
+        }
+    }
+
+    private IEnumerator Die()
+    {
+        Debug.Log("<color=Red>Change State : Die</color>");
+
+        while (true)
+        {
+            Debug.Log("Die State");
+            yield return null;
+        }
     }
 }
