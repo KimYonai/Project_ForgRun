493e5a61aec5de02691b349085cf539efface35f
YunHwan Kim <89112959+KimYonai@users.noreply.github.com>
2025-04-09 10:41:17 +0900

    [Feat] 플레이어 피격 기능 구현
    - 피격 시 1초동안 무적 상태가 되어 피격되지 않도록 구현했습니다.
    - 체력이 0이 되면 사망하도록 구현했습니다.

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public enum PlayerState { Idle, Jump, Slide, Damage, Die }      // 플레이어의 상태 열거형
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerState state;                      // 플레이어의 상태 열거형을 받아오기 위한 변수
-    private Coroutine currentState;                 // 현재 상태 코루틴 변수
-    [SerializeField] private Rigidbody2D rigid;     // 플레이어의 rigidbody
-    [SerializeField] private float jumpForce;       // 플레이어의 점프력
+    [SerializeField] private PlayerModel model;
 
-    [SerializeField] private Transform groundCheck;
-    private float rayLength = 0.6f;
-    [SerializeField] private LayerMask groundLayer;
+    private PlayerState state;                              // 플레이어의 상태 열거형을 받아오기 위한 변수
+    private Coroutine currentState;                         // 현재 상태 코루틴 변수
+    [SerializeField] private Rigidbody2D rigid;             // 플레이어의 rigidbody
 
-    [SerializeField] private BoxCollider2D playerCollider;
-    private float slideHeight = 0.4f;
-    private float colliderHeight;
-    private Vector2 colliderOffset;
+    [SerializeField] private Transform groundCheck;         // 땅에 닿아있는 위치 체크용 Transform 변수
+    private float rayLength = 0.6f;                         // 레이캐스트 광선 길이
+    [SerializeField] private LayerMask groundLayer;         // 땅을 구분짓기 위해 Ground 레이어 체크용 변수
+
+    [SerializeField] private BoxCollider2D playerCollider;  // 플레이어의 콜라이더
+    private float slideHeight = 0.4f;                       // 슬라이드 시 높이
+    private float colliderHeight;                           // 콜라이더의 높이
+
+    // TODO : 추후 에셋 적용 시 플레이어의 기본 오프셋을 발 밑으로 설정 후 오프셋 관련 코드는 삭제
+    private Vector2 colliderOffset;                         // 콜라이더의 오프셋
+
+    private bool isInvincible = false;                      // 플레이어가 무적 상태인지 체크하는 변수
 
     private void Awake()
     {
@@ -44,6 +50,7 @@ public class PlayerController : MonoBehaviour
         {
             if (state != PlayerState.Slide && IsGrounded()) { ChangeState(PlayerState.Slide); }
         }
+        else if (Input.GetKeyDown(KeyCode.D)) { ChangeState(PlayerState.Damage); }
         else if (Input.GetKeyDown(KeyCode.R)) { ChangeState(PlayerState.Die); }
     }
 
@@ -88,7 +95,7 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("<color=Yellow>Change State : Jump</color>");
         // 위쪽 방향으로 jumpForce만큼 점프
-        rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.up * model.JumpForce, ForceMode2D.Impulse);
 
         // IsGrounded의 반환값이 false일 때
         while (!IsGrounded())
@@ -136,14 +143,39 @@ public class PlayerController : MonoBehaviour
     /// <returns></returns>
     private IEnumerator Damage()
     {
+        float invincibleTime = 1f;  // 피격 시 무적 시간
+        float timer = 0f;           // 무적 시간 타이머
+
+        // 무적 상태일 경우 아무 것도 하지 않음
+        if (isInvincible)
+        {
+            Debug.Log("무적 상태입니다");
+            yield break;
+        }
+
         Debug.Log("<color=Blue>Change State : Damage</color>");
 
-        while (true)
+        // 피격 시 무적 상태로 변경하고 플레이어의 체력을 1 감소
+        isInvincible = true;
+        model.HP -= 1;
+
+        // 플레이어의 체력이 0 이하가 되면 Die 상태로 변경
+        if (model.HP <= 0)
         {
-            // TODO : Damage 상태일 때 진행할 행동 기능 추가
-            Debug.Log("Damage State");
+            ChangeState(PlayerState.Die);
+            yield break;
+        }
+
+        // 타이머가 무적 시간(1초)보다 작은 동안에 timer을 deltatime만큼 더하기
+        while (timer < invincibleTime)
+        {
+            timer += Time.deltaTime;
             yield return null;
         }
+
+        // 무적 상태를 해제하고 Idle 상태로 전환
+        isInvincible = false;
+        ChangeState(PlayerState.Idle);
     }
 
     /// <summary>
@@ -162,6 +194,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 플레이어가 땅에 닿아 있는지 체크하는 기능의 함수
+    /// </summary>
+    /// <returns></returns>
     private bool IsGrounded()
     {
         RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, rayLength, groundLayer);
--- Assets/Scripts/Character/PlayerModel.cs
+++ Assets/Scripts/Character/PlayerModel.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int hp;
+    public int HP { get { return hp; } set { hp = value; } }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private float jumpForce;
+    public float JumpForce { get { return jumpForce; } set { jumpForce = value; } }
+
+
 }
